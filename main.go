17c41a87f567beda65afc20c9a98f404273461d6
jramirez09 <jramirez12@luc.edu>
2022-08-09 11:42:50 -0600

    Proof of work

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 )
 
+//derive the Hash from the block's data & previous Hash, and other meta data
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -16,21 +17,35 @@ type BlockChain struct {
 	blocks []*Block
 }
 
+//flow 4
 func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	// This will join our previous block's relevant info with the new blocks
-	hash := sha256.Sum256(info)
+	//2Dimensional slice of bytes
+	//ask
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	//This performs the actual hashing algorithm
+	hash := sha256.Sum256(info)
+	//placeholder to show how hash changes as the data changes
 	b.Hash = hash[:]
 }
 
+//flow 3
+//takes in astring of Data,and the prevHash, and returns a pointer to a block
 func createBlock(data string, prevHash []byte) *Block {
+	//using the block constructor
+	//hash fields empty slice of bytes
+	//Data field taking the data string and  convert into a slice of bytes
+	//prevHash is prevHash
 	block := &Block{[]byte{}, []byte(data), prevHash}
 
+	//flow 4
 	block.DeriveHash()
+
+	//flow 5
 	return block
 }
 
+//takes in the pointer from our BlockChain
 func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.blocks[len(chain.blocks)-1]
 	new := createBlock(data, prevBlock.Hash)
@@ -38,16 +53,24 @@ func (chain *BlockChain) AddBlock(data string) {
 
 }
 
+//return createBlock call with Data for Genesis block, and an empty previous Hash
+//flow 2
 func Genesis() *Block {
+	//flow 3: create block call
 	return createBlock("Genesis", []byte{})
 }
 
+//return a reference to the blockchain
+//inside of it is  an array of Blocks
+//with a call to the Genesis function
+//flow:1
 func InitBlockChain() *BlockChain {
+	//flow 2: Genesis Function
 	return &BlockChain{[]*Block{Genesis()}}
 }
 
 func main() {
-
+	//flow: 1
 	chain := InitBlockChain()
 
 	chain.AddBlock("first block after genesis")
